a09b261613c5617e4224b7783b041b184f66df0f
v_liangqin <v_liangqin@tencent.com>
2021-01-22 17:01:26 +0800

    mysql

--- server/schema/init-models.js
+++ server/schema/init-models.js
@@ -1,14 +1,11 @@
 var DataTypes = require("sequelize").DataTypes;
-var _token = require("./token");
 var _user = require("./user");
 
 function initModels(sequelize) {
-  var token = _token(sequelize, DataTypes);
   var user = _user(sequelize, DataTypes);
 
 
   return {
-    token,
     user,
   };
 }
